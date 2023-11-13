255e7abac89e73a0afe9e052170aac59f3103851
Destanto <destantomy8@gmail.com>
2023-11-13 17:14:48 +0700

    edit cors

--- backend/sources/server.js
+++ backend/sources/server.js
@@ -3,13 +3,19 @@ const connectDB = require('./database/connections');
 require('dotenv').config();
 const routers = require('./routes/routes');
 const cors = require('cors');
+const corsOptions = {
+  origin: 'https://render-i1tx.vercel.app/',
+  methods: 'GET,PUT,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
 // express app
 const app = express();
 const port = process.env.port || 3000;
 
 // cors
-app.use(cors());
+app.use(cors(corsOptions));
 
 // middleware
 app.use(express.json());
